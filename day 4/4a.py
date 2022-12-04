464e903d61fb9185013db21a2002b274d2fa978e
danielletrinh <56095472+danielletrinh@users.noreply.github.com>
2022-12-04 11:46:00 -0800

    day 4

---
+++ day 4/4a.py
@@ -0,0 +1,17 @@
+# Part 1: In how many assignment pairs does one range fully contain the other? [ 657 ]
+
+with open('4_data.txt', 'rt') as data:
+    data_list = [line.strip() for line in data.readlines()]
+for i in range(len(data_list)):
+    data_list[i] = data_list[i].split(',')
+    data_list[i][0] = list(map(int, data_list[i][0].split('-')))
+    data_list[i][1] = list(map(int, data_list[i][1].split('-')))
+
+total = 0
+for item in data_list:
+    if item[0][0] in range(item[1][0], item[1][1] + 1) and item[0][1] in range(item[1][0], item[1][1] + 1):
+        total += 1
+    elif item[1][0] in range(item[0][0], item[0][1] + 1) and item[1][1] in range(item[0][0], item[0][1] + 1):
+        total += 1
+
+print(total)
